2962d0195f27585c26a169972c4cd7c06872f0cb
Jefferson455 <jeffersonguzman167@gmail.com>
2026-05-14 12:27:31 -0500

    feat: button 'Exportar' now is working perfectly

--- src/app/components/pages/dashboard-services/dashboard-services.ts
+++ src/app/components/pages/dashboard-services/dashboard-services.ts
@@ -3,10 +3,13 @@ import { ServicesService } from '../../../core/services/services.service';
 import { Service } from '../../../core/interfaces/service.interface';
 import { CommonModule } from '@angular/common';
 import { UsuarioStorage } from '../../../core/interfaces/customer.interface';
+import { Exportdataexcel, ExportModalStatus } from '../../shared/exportdataexcel/exportdataexcel';
+import * as ExcelJS from 'exceljs';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-dashboard-services',
-  imports: [CommonModule],
+  imports: [Exportdataexcel, CommonModule],
   templateUrl: './dashboard-services.html',
   styleUrl: './dashboard-services.scss',
 })
@@ -22,6 +25,12 @@ export class DashboardServices implements OnInit {
   rolId: number | null = null;
   userId: number | null = null;
   userName = '';
+
+  //* Variables to excel
+  showExportServicesModal = false;
+  exportServicesStatus: ExportModalStatus = 'confirm';
+  exportSuccessMessage = '';
+  exportErrorMessage = '';
 
   ngOnInit(): void {
     this.loadUserRole();
@@ -101,5 +110,110 @@ export class DashboardServices implements OnInit {
       this.rolUser = 'Sin rol';
       this.rolId = null;
     }
+  }
+
+  //* Method to export excel in pc
+  private async exportServicesToExcel(): Promise<void> {
+    const workbook = new ExcelJS.Workbook();
+    const worksheet = workbook.addWorksheet('Motos');
+
+    worksheet.columns = [
+      { header: 'ID', key: 'id', width: 10 },
+      { header: 'Moto Placa', key: 'placa', width: 18 },
+      { header: 'Nombre tecnico', key: 'nameMechanic', width: 20 },
+      { header: 'Descripción', key: 'description', width: 35 },
+      { header: 'Kilometraje', key: 'km', width: 15 },
+      { header: 'Costo', key: 'cost', width: 18 },
+      { header: 'Fecha', key: 'serviceDate', width: 18 },
+      { header: 'Estado', key: 'status', width: 14 },
+    ];
+
+    this.services.forEach((service) => {
+      worksheet.addRow({
+        id: service.id,
+        placa: service.motoPlaca || 'No hay dato registrado',
+        nameMechanic: service.nombreUsuario ?? 'N/A',
+        description: service.descripcion || 'No hay dato registrado',
+        km: service.kilometraje || 'No hay dato registrado',
+        cost: service.costo || 'No hay dato registrado',
+        serviceDate: service.fecha || 'No hay dato registrado',
+        status: service.estado ?? 'N/A',
+      });
+    });
+
+    worksheet.getRow(1).font = { bold: true };
+
+    worksheet.eachRow((row) => {
+      row.eachCell((cell) => {
+        cell.alignment = { vertical: 'middle', horizontal: 'left' };
+      });
+    });
+
+    const buffer = await workbook.xlsx.writeBuffer();
+
+    const fileName = `motos_tallermotos_${this.getTodayFileName()}.xlsx`;
+
+    saveAs(
+      new Blob([buffer], {
+        type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+      }),
+      fileName
+    );
+  }
+
+  //* Method for get the date to excel
+  private getTodayFileName(): string {
+    const today = new Date();
+
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const day = String(today.getDate()).padStart(2, '0');
+
+    return `${year}-${month}-${day}`;
+  }
+
+  //* Call the modal export to excel
+  openExportServicesModal(): void {
+    this.exportServicesStatus = 'confirm';
+    this.exportSuccessMessage = '';
+    this.exportErrorMessage = '';
+    this.showExportServicesModal = true;
+  }
+
+  //* End the modal export in excel
+  closeExportServicesModal(): void {
+    this.showExportServicesModal = false;
+    this.exportServicesStatus = 'confirm';
+    this.exportSuccessMessage = '';
+    this.exportErrorMessage = '';
+  }
+
+  //* Modal confirm to export excel
+  confirmExportServices(): void {
+    if (this.services.length === 0) {
+      this.exportServicesStatus = 'error';
+      this.exportErrorMessage = 'No hay motos registradas para exportar.';
+      this.cdr.detectChanges();
+      return;
+    }
+
+    this.exportServicesStatus = 'loading';
+    this.cdr.detectChanges();
+
+    setTimeout(() => {
+      try {
+        this.exportServicesToExcel();
+
+        this.exportSuccessMessage = `Se exportaron ${this.services.length} motos correctamente.`;
+        this.exportServicesStatus = 'success';
+        this.cdr.detectChanges();
+      } catch (error) {
+        console.error('Error exportando motos:', error);
+
+        this.exportErrorMessage = 'No se pudo generar el archivo Excel de motos.';
+        this.exportServicesStatus = 'error';
+        this.cdr.detectChanges();
+      }
+    }, 900);
   }
 }
